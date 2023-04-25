848c6d0f25592261abbfeef315857d1cc469848d
aniqu18 <niko030.nk@gmail.com>
2023-04-26 00:59:18 +0200

    Created BallistaMachine class and test

---
+++ hero-common/src/main/java/WarMachines/BallistaMachine.java
@@ -0,0 +1,24 @@
+package WarMachines;
+
+/*
+Class for ballista from war machines. Describes all behaviours of ballista
+ */
+
+public class BallistaMachine {
+
+
+    public BallistaMachine() {
+    }
+
+    // Method uses given attack value to calculate damage wich will be applied on creature attacked by ballisa
+    public int caclculateDamageFromBallista(int attack){
+        int lowerBoundary = 2 * (attack + 11);
+        int upperBoundary = 3 * (attack + 11);
+        float damage = (float) getRandomNumber(lowerBoundary, upperBoundary);
+        return Math.round(damage);
+    }
+
+    public double getRandomNumber(int min, int max) {
+        return ((Math.random() * (max - min)) + min);
+    }
+}
---
+++ hero-common/src/test/java/WarMachines/BallistaMachineTest.java
@@ -0,0 +1,17 @@
+package WarMachines;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallistaMachineTest {
+
+    @Test
+    void caclculateDamageFromBallista() {
+    }
+
+    @Test
+    void getRandomNumber() {
+        
+    }
+}
