838a76b19a54eaad95f476063e0a2e1a11aa1cf3
aniqu18 <niko030.nk@gmail.com>
2023-04-30 16:04:38 +0200

    Tests for new turn queue and updates for ballista
    The BallistaMachine class now has new fields and an updated damage calculator. The calculator now checks whether a hero has an artillery skill.

---
+++ battle-model/src/test/java/pl/psi/NewTurnQueueWithWarMachinesTest.java
@@ -0,0 +1,60 @@
+package pl.psi;
+
+import WarMachines.WarMachine;
+import WarMachines.WarMachineStats;
+import org.junit.jupiter.api.Test;
+import pl.psi.creatures.Creature;
+import pl.psi.creatures.CreatureStats;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewTurnQueueWithWarMachinesTest {
+
+    final Creature creature1 = new Creature.Builder().statistic( CreatureStats.builder()
+                    .build() )
+            .build();
+    final Creature creature2 = new Creature.Builder().statistic( CreatureStats.builder()
+                    .build() )
+            .build();
+    final Creature creature3 = new Creature.Builder().statistic( CreatureStats.builder()
+                    .build() )
+            .build();
+
+    final WarMachine warMachine1 = new WarMachine.Builder().statistic(WarMachineStats.builder().build()).build();
+    final WarMachine warMachine2 = new WarMachine.Builder().statistic(WarMachineStats.builder().build()).build();
+    final WarMachine warMachine3 = new WarMachine.Builder().statistic(WarMachineStats.builder().build()).build();
+    NewTurnQueueWithWarMachines newTurnQueueWithWarMachines = new NewTurnQueueWithWarMachines( List.of( creature1, creature2 ), List.of( creature3 ), List.of(warMachine1, warMachine2), List.of(warMachine3) );
+
+    @Test
+    void getCurrentCreature() {
+        assertEquals(newTurnQueueWithWarMachines.getCurrentCreature(), creature1);
+    }
+
+    @Test
+    void getCurrentWarMachine() {
+        assertEquals(newTurnQueueWithWarMachines.getCurrentWarMachine(), warMachine1);
+    }
+
+    @Test
+    void shouldGoToNextCreatureAndWarMachine() {
+        assertEquals(newTurnQueueWithWarMachines.getCurrentCreature(), creature1);
+        assertEquals(newTurnQueueWithWarMachines.getCurrentWarMachine(), warMachine1);
+
+        newTurnQueueWithWarMachines.next();
+
+        assertEquals(newTurnQueueWithWarMachines.getCurrentCreature(), creature2);
+        assertEquals(newTurnQueueWithWarMachines.getCurrentWarMachine(), warMachine2);
+
+        newTurnQueueWithWarMachines.next();
+
+        assertEquals(newTurnQueueWithWarMachines.getCurrentCreature(), creature3);
+        assertEquals(newTurnQueueWithWarMachines.getCurrentWarMachine(), warMachine3);
+
+        newTurnQueueWithWarMachines.next();
+
+        assertEquals(newTurnQueueWithWarMachines.getCurrentCreature(), creature1);
+        assertEquals(newTurnQueueWithWarMachines.getCurrentWarMachine(), warMachine1);
+    }
+}
