668d95d872a60982504c3a3316da75a8935947b4
MikolajTym <mikolaj.tym@gmail.com>
2023-05-10 12:16:13 +0200

    HallBuilding body.

--- economy-model/src/main/java/pl/psi/buildings/HallBuilding.java
+++ economy-model/src/main/java/pl/psi/buildings/HallBuilding.java
@@ -1,4 +1,30 @@
 package pl.psi.buildings;
 
-public class HallBuilding {
+import pl.psi.mapElements.Castle;
+import pl.psi.mapElements.Resource;
+
+import java.util.ArrayList;
+
+public class HallBuilding extends Building{
+
+    public HallBuilding(String name, Type type, int id, Castle.FractionType fraction) {
+        super(name, type, id, fraction);
+    }
+
+    private ArrayList<Building> buildingsAwailable = new ArrayList<>();
+
+    public ArrayList<Building> getBuildingsAwailable() {
+        return buildingsAwailable;
+    }
+
+    public void setBuildingsAwailable(ArrayList<Building> buildingsAwailable) {
+        this.buildingsAwailable = buildingsAwailable;
+    }
+
+    public void buyBuilding(int id){
+        for(Building building: buildingsAwailable){
+
+        }
+        this.buildingsAwailable.get(id);
+    }
 }
