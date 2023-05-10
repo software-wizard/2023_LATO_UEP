a41043b0d87c76d4f15f2a3113d872de805d6882
MikolajTym <mikolaj.tym@gmail.com>
2023-05-10 12:20:09 +0200

    Recruitment body.

--- economy-model/src/main/java/pl/psi/buildings/RecruitmentBuilding.java
+++ economy-model/src/main/java/pl/psi/buildings/RecruitmentBuilding.java
@@ -1,42 +1,42 @@
 package pl.psi.buildings;
 
 import pl.psi.creatures.EconomyCreature;
-import pl.psi.creatures.EconomyNecropolisFactory;
+import pl.psi.creatures.EconomyFactory;
 import pl.psi.mapElements.Castle;
 import pl.psi.mapElements.Resource;
 
 
 public class RecruitmentBuilding extends Building{
-    public RecruitmentBuilding(String name, Type type, int spot, Resource cost, Castle.FractionType fraction) {
-        super(name, type, spot, cost, fraction);
+    public RecruitmentBuilding(String name, Type type, int id, Resource cost, Castle.FractionType fraction) {
+        super(name, type, id, cost, fraction);
     }
 
     private int tier;
     private EconomyCreature creaturesToRecruit;
+    EconomyFactory factory = new EconomyFactory();
 
-    public RecruitmentBuilding(String name, Type type, int spot, Resource cost, int tier, Castle.FractionType fraction) {
-        super(name, type, spot, cost, fraction);
+    public RecruitmentBuilding(String name, Type type, int id, Resource cost, int tier, Castle.FractionType fraction) {
+        super(name, type, id, cost, fraction);
         this.tier = tier;
     }
 
-    public RecruitmentBuilding(String name, Type type, int spot, int tier, Castle.FractionType fraction) {
-        super(name, type, spot, fraction);
+    public RecruitmentBuilding(String name, Type type, int id, int tier, Castle.FractionType fraction) {
+        super(name, type, id, fraction);
         this.tier = tier;
-        this.creaturesToRecruit = createCreatures(this.getFraction(), this.tier, 20);
+        this.creaturesToRecruit = factory.create(this.getFraction(), false, tier, 20);
     }
 
 
-    //inna nazwa metody, stworzyć fabryke jako pole zamiast tej metody
-    private EconomyCreature createCreatures(Castle.FractionType fraction, int tier, int amount){
-        switch(fraction){
-            case TOWER:
-                break;
-            case NECROPOLIS:
-                EconomyNecropolisFactory factory = new EconomyNecropolisFactory();
-                return factory.create(false, tier, amount);
-        }
-        return null;
-    }
+//    private EconomyCreature createCreatures(Castle.FractionType fraction, int tier, int amount){
+//        switch(fraction){
+//            case TOWER:
+//                break;
+//            case NECROPOLIS:
+//                EconomyFactory factory = new EconomyFactory();
+//                return factory.create(this.getFraction(), false, tier, amount);
+//        }
+//        return null;
+//    }
 
     public String getCreaturesName(){
         return this.creaturesToRecruit.getName();
@@ -45,10 +45,8 @@ public class RecruitmentBuilding extends Building{
 
     public EconomyCreature takeCreaturesFromBuilding(int amount){
         if(amount <= this.creaturesToRecruit.getAmount()){
-            EconomyCreature creaturesToArmy = creaturesToRecruit;
-            creaturesToArmy.setAmount(amount);
-//            this.getCreaturesToRecruit().setAmount(this.getCreaturesToRecruit().getAmount()-amount);
-            return creaturesToArmy;
+            creaturesToRecruit.setAmount(this.creaturesToRecruit.getAmount() - amount);
+            return factory.create(this.getFraction(), false, tier, amount);
         }
         return null;
     }
