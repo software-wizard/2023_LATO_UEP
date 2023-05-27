aec50865754fd5750b586a940bca90e2ccb32e1d
pwieczorek <pwieczorek@psi.pl>
2023-05-27 11:12:52 +0200

    merge economy

--- economy-model/src/main/java/pl/psi/hero/EconomyHero.java
+++ economy-model/src/main/java/pl/psi/hero/EconomyHero.java
@@ -3,18 +3,19 @@ package pl.psi.hero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import pl.psi.artifacts.Artifact;
 import pl.psi.buildings.RecruitmentBuilding;
 import pl.psi.creatures.EconomyCreature;
 import pl.psi.mapElements.Castle;
-import pl.psi.artifacts.Artifact;
 import pl.psi.mapElements.MapElement;
 import pl.psi.player.PlayerResources;
+
 import java.util.ArrayList;
 
 
-@AllArgsConstructor
 @Builder
 @Getter
+@AllArgsConstructor
 public class EconomyHero implements MapElement {
 
 
@@ -22,17 +23,20 @@ public class EconomyHero implements MapElement {
 
     private HeroStatistics heroStatistics;
 
-    public static Castle.FractionType Fraction;
+    public Castle.FractionType fraction; // @Q - why static?!
     private HeroEquipment heroEquipment;
     private ArrayList<EconomyCreature> heroArmy;
 
-
-    public EconomyHero(HeroStatistics aHeroStatistics,ArrayList<EconomyCreature> aHeroArmy,HeroEquipment aHeroEquipment) {
-        this.heroStatistics = aHeroStatistics;
-        this.heroEquipment = aHeroEquipment;
-        this.heroArmy = aHeroArmy;
+    public EconomyHero(HeroStatistics aHeroStatistics, Castle.FractionType aFraction, ArrayList<EconomyCreature> aHeroArmy) {
+        heroStatistics = aHeroStatistics;
+        fraction = aFraction;
+        heroArmy = aHeroArmy;
+        heroEquipment = new HeroEquipment();
     }
 
+    public EconomyHero(HeroStatistics aStats, ArrayList<EconomyCreature> aCreatures, HeroEquipment aEq) {
+        this(aStats, Castle.FractionType.NECROPOLIS, aEq, aCreatures);
+    }
 
     @Override
     public boolean isInteractive() {
@@ -45,13 +49,13 @@ public class EconomyHero implements MapElement {
         // TODO battle if enemy hero
     }
 
-    public void addCreaturesToArmy(RecruitmentBuilding building, int amount, PlayerResources resources){
-        int creaturesCost = building.getCreaturesToRecruit().getAmount()*building.getCreaturesToRecruit().getGoldCost();
+    public void addCreaturesToArmy(RecruitmentBuilding building, int amount, PlayerResources resources) {
+        int creaturesCost = building.getCreaturesToRecruit().getAmount() * building.getCreaturesToRecruit().getGoldCost();
 
-        if(creaturesCost<resources.getGold()){
+        if (creaturesCost < resources.getGold()) {
             EconomyCreature armyCreature = building.takeCreaturesFromBuilding(amount);
             heroArmy.add(armyCreature);
-            resources.setGold(resources.getGold()-creaturesCost);
+            resources.setGold(resources.getGold() - creaturesCost);
         }
     }
 
@@ -71,7 +75,7 @@ public class EconomyHero implements MapElement {
     }
 
     public ArrayList<EconomyCreature> addCreature(EconomyCreature economyCreature) {
-        heroArmy.add( economyCreature);
+        heroArmy.add(economyCreature);
         return heroArmy;
     }
 
